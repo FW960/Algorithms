738cc83c88cee544cba337a98d9ced468cd1b455
FW960 <artuninp@gmail.com>
2022-04-15 00:03:34 +0800

    Вычисление Простых Чисел

---
+++ Project/PrimeNum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public static class PrimeNum
+{
+    /// <summary>
+    /// Method checks if input int parameter is prime number. Returns bool value type.
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public static bool IsPrimeNum(int n)
+    {
+        int d = 0; int i = 2;
+
+        if (i < n)
+        {
+            if (n % i == 0)
+                d++;
+            else
+                i++;
+        }
+        if (d == 0)
+            return true;
+        else
+            return false;
+    }
+}
