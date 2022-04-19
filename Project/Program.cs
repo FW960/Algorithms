9c2644833de03da246e95db791bb263b36239a37
FW960 <artuninp@gmail.com>
2022-04-20 04:14:50 +0800

    Написал сортировщик O(n2)/2

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Sorters;
 
 namespace Project
 {
@@ -6,7 +10,53 @@ namespace Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program pr = new Program() + new Program();
+
+            BenchmarkSwitcher.FromAssembly(typeof(Sort).Assembly).Run(args);
+        }
+        public static int[] GenRndIntArray()
+        {
+            Random random = new Random();
+
+            int[] array = new int[100];
+
+            for (int i = 0; i < array.Length; i++)
+                array[i] = random.Next(0, 100000);
+
+            return array;
+        }
+
+        public static Program operator +(Program pr, Program pr1)
+        {
+            return pr + pr1;
+        }
+    }
+
+    public class HalfShakeTest
+    {
+        int [] array = Program.GenRndIntArray();
+        [Benchmark]
+        public void TestHalfSHake()
+        {
+            Sort.HalfShakeSort(array);
+        }
+    }
+    public class BubbleTest
+    {
+        int[] array = Program.GenRndIntArray();
+        [Benchmark]
+        public void TestBubble()
+        {
+            Sort.Bubble(array);
+        }
+    }
+    public class MyFirstSortTest
+    {
+        int[] array = Program.GenRndIntArray();
+        [Benchmark]
+        public void TestMyFirstSort()
+        {
+            Sort.MyFirstSort(array);
         }
     }
 }
---
+++ Project/Sorter.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Sorters
+{
+    public static class Sort
+    {
+        public static int[] HalfShakeSort(int[] array)
+        {
+            int temp = int.MinValue;
+
+            int index = 0;
+
+            int lastNum = 0;
+
+            for (int i = 0; i < array.Length - lastNum; i++) // O(n2)/2
+            {
+                if (lastNum == array.Length - 1)
+                    break;
+                if (array[i] > temp)
+                {
+                    temp = array[i];
+                    index = i;
+                }
+                if (i == array.Length - 1 - lastNum)
+                {
+                    array[index] = array[i];
+
+                    array[i] = temp; lastNum++; i = -1; temp = int.MinValue;
+
+                }
+            }
+
+            return array;
+        }
+        public static int[] MyFirstSort(int[] array)
+        {
+            int temp;
+
+            for (int i = 0; array.Length > i; i++)
+                for (int j = 0; j < array.Length; j++)
+                {
+
+                    if (array[i] < array[j])
+                    {
+                        temp = array[i];
+
+                        array[i] = array[j];
+
+                        array[j] = temp;
+
+                    }
+                }
+            return array;
+        }
+        public static int[] Bubble(int[] array)
+        {
+            int temp;
+
+            for (int j = 0; j < array.Length; j++)
+            {
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        temp = array[i];
+
+                        array[i] = array[i + 1];
+
+                        array[i + 1] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+        public static int[] Shake(int[] array)
+        {
+            throw new NotImplementedException();
+        }
+        public static int[] GenRndIntArray()
+        {
+            Random random = new Random();
+
+            int[] array = new int[100000];
+
+            for (int i = 0; i < array.Length; i++)
+                array[i] = random.Next(0, 100000);
+
+            return array;
+        }
+    }
+}
